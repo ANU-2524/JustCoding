2b3ede76d8457602311c30675db6e2a54c6b5e56
Siddhesh gite <siddheshgite154@gmail.com>
2026-01-03 21:40:40 +0530

    Ui improvement and components position fixing with dark/light mode

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,47 +1,47 @@
 // src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./components/AuthContext";
+import { ThemeProvider } from "./components/ThemeContext";
 import LoginPage from "./components/LoginPage";
 import MainEditor from "./components/MainEditor";
 import ProtectedRoute from "./components/ProtectedRoute";
-import HomePage from "./components/HomePage"; // Optional
-import JoinRoom from "./components/JoinRoom"; // 👈 added
-import LiveRoom from "./components/LiveRoom"; // 👈 added
+import HomePage from "./components/HomePage";
+import JoinRoom from "./components/JoinRoom";
+import LiveRoom from "./components/LiveRoom";
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
+    <ThemeProvider>
+      <AuthProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
 
-          {/* Main personal editor */}
-          <Route
-            path="/editor"
-            element={<MainEditor />}
-          />
+            {/* Main personal editor */}
+            <Route path="/editor" element={<MainEditor />} />
 
-          {/* Collaborative editor */}
-          <Route
-            path="/live"
-            element={
-              <ProtectedRoute>
-                <JoinRoom />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/live/:roomId"
-            element={
-              <ProtectedRoute>
-                <LiveRoom />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
-      </Router>
-    </AuthProvider>
+            {/* Collaborative editor */}
+            <Route
+              path="/live"
+              element={
+                <ProtectedRoute>
+                  <JoinRoom />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/live/:roomId"
+              element={
+                <ProtectedRoute>
+                  <LiveRoom />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </Router>
+      </AuthProvider>
+    </ThemeProvider>
   );
 }
 
