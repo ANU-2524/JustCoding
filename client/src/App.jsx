a913954accb05bbd52386c0e2429cd088ec6fb9e
Gupta-02 <Gupta-02@users.noreply.github.com>
2026-01-26 14:39:07 +0530

    Implemented TP

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -29,8 +29,10 @@ const ContestLeaderboard = lazy(() => import("./components/ContestLeaderboard"))
 const ContestManagement = lazy(() => import("./components/ContestManagement"));
 const ProgressExport = lazy(() => import("./components/ProgressExport"));
 const BadgesPage = lazy(() => import("./components/BadgesPage"));
-const UserManagement = lazy(() => import("./components/UserManagement"));
+// const UserManagement = lazy(() => import("./components/UserManagement"));
 const DebugHelper = lazy(() => import("./components/DebugHelper"));
+const TutorialsPage = lazy(() => import("./components/TutorialsPage"));
+const TutorialView = lazy(() => import("./components/TutorialView"));
 
 const RoomManagement = lazy(() => import("./components/RoomManagement"));
 
@@ -101,6 +103,10 @@ function App() {
                 <Route path="/challenges" element={<Challenges />} />
                 <Route path="/challenges/:slug" element={<ChallengeSolve />} />
                 
+                {/* Tutorials */}
+                <Route path="/tutorials" element={<TutorialsPage />} />
+                <Route path="/tutorials/:slug" element={<TutorialView />} />
+                
                 {/* Contests */}
                 <Route path="/contests" element={<Contests />} />
                 <Route path="/contests/:slug" element={<ContestDetail />} />
@@ -112,7 +118,7 @@ function App() {
                 <Route path="/progress" element={<Analytics />} />
                 <Route path="/export" element={<ProgressExport />} />
                 <Route path="/badges" element={<BadgesPage />} />
-                <Route path="/admin/users" element={<UserManagement />} />
+                {/* <Route path="/admin/users" element={<UserManagement />} /> */}
                 <Route path="/debug" element={<DebugHelper />} />
 
                 <Route path="/admin/rooms" element={<RoomManagement />} />
